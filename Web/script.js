6a481dc1f15f8b4607e6bfdaf7265b3969a3300b
Gustavo Gutierrez <playsslinky@gmail.com>
2022-07-11 16:39:12 -0300

    update main

--- Web/script.js
+++ Web/script.js
@@ -1,8 +1,24 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "topnav";
+        x.className = "topnav";
     }
 }
+
+var screenSize = window.matchMedia("((max-width: 600px))")
+
+function openNav() {
+        document.getElementById("mySidenav").style.width = "295px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+const clearInput = () => {
+    const input = document.getElementsByTagName("input")[0];
+    input.value = "";
+}
+
