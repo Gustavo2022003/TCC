54d12a79b5c031ce4228f11b0beb264ecac47139
João Henrique <joaojho7@gmail.com>
2022-08-14 20:21:39 -0300

    BAGUI TA FICANDO PICA MENOR
    ta daora

--- TCC-React/Pages/Register.js
+++ TCC-React/Pages/Register.js
@@ -15,88 +15,109 @@ import * as Animatable from 'react-native-animatable';
 import { useNavigation } from '@react-navigation/native';
 import { ScrollView } from 'react-native-gesture-handler';
 import logo from './Images/Foodio.png';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
 export default function Register() {
-
-  const [name, setName]=useState(null)
-  const [username, setUser]=useState(null)
-  const [email, setEmail]=useState('');
-  const [emailValidError, setEmailValidError] = useState('');
-  const [password, setPassword]=useState(null)
-  const [confirmPassword, setConfirmPassword]=useState(null)
-
+  const UserInfo = {fullname: '', username:'', email: '', password: '', confirmPassword:'' }
+  const [username,setUsername] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [fullname,setfullname] = useState('');
   //Validation E-mail
-  const handleValidEmail = val => {
-    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-    
-        if (val.length === 0) {
-          setEmailValidError('Insira um endereço de email');
-        } else if (reg.test(val) === false) {
-          setEmailValidError('Digite um endereço de email valido!');
-        } else if (reg.test(val) === true) {
-          setEmailValidError('');
-        }
-    };
-    
-
 
 
   // Function to Register
-  async function sendFormRegister(){
-    let response= await fetch('http://192.168.0.108:3000/register',{
+  async function sendFormRegister(values){
+    /*let response= await fetch('http://192.168.0.108:3000/register',{
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        usernamename: username,
-        password: password
+        fullname: UserInfo.fullname,
+        username: UserInfo.username,
+        email: UserInfo.email,
+        password: UserInfo.password,
       })
-    });
-    let json=await response.json();
+    });*/
+    console.log(values.fullname);
+    console.log(values.username);
+    console.log(values.email);
+    console.log(values.password);
+    /*let json=await response.json();
     if(json === 'error'){
       console.log('Ocorreu um Erro')
     }else{
       console.log('Usuario Cadastrado')
-    };
+    };*/
   }
 
+  const registerSchema = yup.object().shape({
+    fullname: yup.string().trim().min(3, 'Invalid Name').required('Full Name is Required'),
+    username: yup.string().trim().min(3, 'Username too short!').required('Username is Required'),
+    email: yup.string().email('Invalid email').required('Email is Required'),
+    password: yup.string().trim().min(8,'Password is too short!').required('Password is Required'),
+    confirmPassword: yup.string().equals([yup.ref('password'),null], 'Password does not match!').required('Password confirmation is Required')
+  });
 
 
     return (
       <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
       >
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-    <Animatable.View animation='fadeInUp' style={styles.container}>
-        <View style={styles.logo}>
-        <Image source={logo} style={styles.logoimg}/>
-        </View>
-        <View style={styles.formBg}>
-            <View style={styles.forms}>
-            <Text style={styles.title}>Register</Text>
-              <ScrollView style={{width: '100%'}}>
-                <View style={styles.bottom}>
-                <TextInput style={styles.input} placeholder='Full name' onChangeText={text=>setName(text)}/>
-                <TextInput style={styles.input} placeholder='Username' onChangeText={text=>setUser(text)}/>
-                <TextInput style={styles.input} placeholder='E-mail' value={email} onChangeText={value => {setEmail(value); handleValidEmail(value);}}/>
-                <View>
-                  {emailValidError ? <Text style={styles.invalidEmail}>{emailValidError}</Text> : null}
-                </View>
-                <TextInput style={styles.input} placeholder='Password' secureTextEntry={true} onChangeText={text=>setUser(text)}/>
-                <TextInput style={styles.input} placeholder='Confirm Password' secureTextEntry={true} onChangeText={text=>setUser(text)}/>
-                <TouchableOpacity style={styles.RegButton}>
-                    <Text style={styles.RegText}>Register</Text>
-                </TouchableOpacity>
-                </View>
-                </ScrollView> 
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <Animatable.View animation='fadeInUp' style={styles.container}>
+              <View style={styles.logo}>
+              <Image source={logo} style={styles.logoimg}/>
               </View>
-        </View> 
-    </Animatable.View>
-    </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+              <View style={styles.formBg}>
+                  <View style={styles.forms}>
+                    <Text style={styles.title}>Register</Text>
+                      <ScrollView style={{width: '100%'}}>
+                        <View style={styles.bottom}>
+                        <Formik
+                          initialValues={UserInfo}
+                          onSubmit={sendFormRegister}
+                          validationSchema={registerSchema}
+                          >
+                          {({handleSubmit, errors,touched, handleChange, handleBlur, values})=> {
+                            const{fullname, username, email, password,confirmPassword} = values
+                          return (
+                          <>
+                          <TextInput style={styles.input} placeholder='Full name' value={fullname} onBlur={handleBlur('fullname')} onChangeText={handleChange('fullname',UserInfo.fullname)}/>
+                          {touched.fullname && errors.fullname && <Text style={styles.error}>{errors.fullname}</Text>}
+
+                          <TextInput style={styles.input} placeholder='Username' value={username} onBlur={handleBlur('username')} onChangeText={handleChange('username',UserInfo.username)}/>
+                          {touched.username && errors.username && <Text style={styles.error}>{errors.username}</Text>}
+
+                          <TextInput style={styles.input} placeholder='E-mail' value={email} onBlur={handleBlur('email')} onChangeText={handleChange('email',UserInfo.email)}/>
+                          {touched.email && errors.email && <Text style={styles.error}>{errors.email}</Text>}
+
+                          <TextInput style={styles.input} placeholder='Password' value={password} onBlur={handleBlur('password')} secureTextEntry={true} onChangeText={handleChange('password')}/>
+                          {touched.password && errors.password && <Text style={styles.error}>{errors.password}</Text>}
+                          
+                          <TextInput style={styles.input} placeholder='Confirm Password' value={confirmPassword} onBlur={handleBlur('confirmPassword')} secureTextEntry={true} onChangeText={handleChange('confirmPassword')}/>
+                          {touched.confirmPassword && errors.confirmPassword && <Text style={styles.error}>{errors.confirmPassword}</Text>}
+                          {errors.fullname || errors.username || errors.email || errors.password || errors.confirmPassword ?
+                          <TouchableOpacity style={styles.RegButtonInvalid} disabled={!Formik.isValid}>
+                              <Text style={styles.RegTextInvalid}>Register</Text> 
+                          </TouchableOpacity>
+                          : <TouchableOpacity style={styles.RegButton} onPress={handleSubmit} disabled={!registerSchema.isValid}>
+                              <Text style={styles.RegText}>Register</Text> 
+                            </TouchableOpacity>}
+                          </>
+                          )}} 
+                        </Formik>
+                        </View>
+                      </ScrollView> 
+                  </View>
+              </View> 
+          </Animatable.View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
   );
 }
 
@@ -149,7 +170,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#EDEDED',
     borderRadius: 18,
   },
-  invalidEmail:{
+  error:{
+    marginTop: '-3%',
     paddingBottom: 10,
     color: 'red',
     fontWeight: '700',
@@ -165,9 +187,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  RegButtonInvalid:{
+    marginTop: '3%',
+    alignSelf:'center',
+    backgroundColor: '#eaeaea',
+    width: '60%',
+    height: 55,
+    borderRadius: 112,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   RegText:{
     fontSize: 26,
     fontWeight: '700',
     color: '#000000',
+  },
+  RegTextInvalid:{
+    fontSize: 26,
+    fontWeight: '700',
+    color: '#999999',
   }
 });
