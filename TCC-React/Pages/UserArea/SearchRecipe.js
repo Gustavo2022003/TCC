b9ef5689c250548224df2727d3caf899b74fadbb
João Henrique <joaojho7@gmail.com>
2022-08-31 22:01:00 -0300

    Isso aqui ta interessante
    Ta daora o bagui

--- TCC-React/Pages/UserArea/SearchRecipe.js
+++ TCC-React/Pages/UserArea/SearchRecipe.js
@@ -53,11 +53,13 @@ export default function SearchRecipe() {
 
 
     async function checkGeral(){
-        var ingredientQuery = ingredients.filter(ingredient => ingredient.quantItem > 0).map(ingredients => {return {...ingredients}});
+        var ingredientQuery = ingredients.filter(ingredient => ingredient.quantItem > 0).map(ingredients => {return [ingredients.id, ingredients.quantItem]})
+        let flatQuery = ingredientQuery.flatMap(ingredients => ingredients)
         /*ingredients.forEach(item => {
             item.quantItem = 0;
         });*/
-        console.log(ingredientQuery);
+
+        console.log(flatQuery);
     }
 
     //Render item to Flat List
